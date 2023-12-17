3226f7b4afcd2c40d114574a762b8cf8ea7783e0
Lurelin <ali.mehdi.jaffer@gmail.com>
2023-12-17 08:51:03 -0500

    dtsgevr

--- cubeRenderer.py
+++ cubeRenderer.py
@@ -1,14 +1,21 @@
 from customMath import Vector3
 
 class polygon:
-    def __init__(self, shapes, colour):
-        self.shapes = shapes
+    def __init__(self, points, colour):
+        self.shape = points
         self.colour = colour
 
 class cube:
     def __init__(self, position, scale):
-        self.position = Vector3(position)
-        self.scale = Vector3(scale)
+        self.position = position
+        self.scale = scale
 
-def cubeToPolys(cube):
-    pass
+def cubeToPolys(cube, topScale):
+    x = cube.position.x
+    y = cube.position.y
+    z = cube.position.z
+    origin = (-z + x, y - z*topScale - x*topScale)
+    middleBottom = (origin[0], origin[0] - cube.scale.y)
+
+theCube = cube(Vector3(0, 0, 0), Vector3(1, 1, 1))
+cubeToPolys(theCube, 0.5)
